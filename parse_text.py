a0a6ae8667ef671bf0fce849fea03a8494c36f64
George Hongkai Sun <sun.ho@husky.neu.edu>
2014-04-15 10:10:08 -0400

    Add exception handling.

--- parse_text.py
+++ parse_text.py
@@ -14,6 +14,7 @@ from string import punctuation
 import csv
 import nltk
 import sys
+import logging
 
 if len(sys.argv) < 2:
     print('[USAGE] %s <CSV INPUT>' % sys.argv[0])
@@ -49,52 +50,55 @@ bow_list = []
 bow_tag_list = []
 # Tag in
 for row in src:
-    rid = int(row['review_id']) - 1
-    # if rid > 100:
-        # break
-    text = reviews[rid]['text']
-    bow_row = {
-        'review_id': rid + 1
-    }
-    bow_tag_row = {
-        'review_id': rid + 1
-    }
-    
-    # Sentence Tokenize
-    sents = nltk.tokenize.sent_tokenize(text)
-    for sent in sents:
-        # Word Tokenize
-        words = nltk.tokenize.word_tokenize(sent)
-        # PoS Tagging
-        tagged_words = nltk.pos_tag(words)
-        # Add in
-        for word in words:
-            word = word.lower()
-            if word not in bow_row:
-                bow_row[word] = 0
-                if word not in dicts:
-                    dicts[word] = [0, 0]
-                dicts[word][1] += 1
-            bow_row[word] += 1
-            dicts[word][0] += 1
-        for tagged_word in tagged_words:
-            tagged_word = list(tagged_word)
-            tagged_word[0] = tagged_word[0].lower()
-            word = encode_tagged(tagged_word)
-            if word not in bow_tag_row:
-                bow_tag_row[word] = 0
-                if word not in dicts_tag:
-                    dicts_tag[word] = [0, 0]
-                dicts_tag[word][1] += 1
-            bow_tag_row[word] += 1
-            dicts_tag[word][0] += 1
-    bow_list.append(bow_row)
-    bow_tag_list.append(bow_tag_row)
-    print('Tokenized %d' % rid)
+    try:
+        rid = int(row['review_id']) - 1
+        # if rid > 100:
+            # break
+        text = reviews[rid]['text']
+        bow_row = {
+            'review_id': rid + 1
+        }
+        bow_tag_row = {
+            'review_id': rid + 1
+        }
+        
+        # Sentence Tokenize
+        sents = nltk.tokenize.sent_tokenize(text)
+        for sent in sents:
+            # Word Tokenize
+            words = nltk.tokenize.word_tokenize(sent)
+            # PoS Tagging
+            tagged_words = nltk.pos_tag(words)
+            # Add in
+            for word in words:
+                word = word.lower()
+                if word not in bow_row:
+                    bow_row[word] = 0
+                    if word not in dicts:
+                        dicts[word] = [0, 0]
+                    dicts[word][1] += 1
+                bow_row[word] += 1
+                dicts[word][0] += 1
+            for tagged_word in tagged_words:
+                tagged_word = list(tagged_word)
+                tagged_word[0] = tagged_word[0].lower()
+                word = encode_tagged(tagged_word)
+                if word not in bow_tag_row:
+                    bow_tag_row[word] = 0
+                    if word not in dicts_tag:
+                        dicts_tag[word] = [0, 0]
+                    dicts_tag[word][1] += 1
+                bow_tag_row[word] += 1
+                dicts_tag[word][0] += 1
+        bow_list.append(bow_row)
+        bow_tag_list.append(bow_tag_row)
+        print('Tokenized %d' % rid)
+    except:
+        logging.exception('Tokenizing Failed %d' % rid)
 src_f.close()
 # Print Stat
-f1 = open('dict.dat', 'w+')
-f2 = open('dict-tag.dat', 'w+')
+f1 = open('dict.dat', 'w+', encoding='utf8')
+f2 = open('dict-tag.dat', 'w+', encoding='utf8')
 print('word\tcount\toccurrence', file=f1)
 print('word\ttag\tcount\toccurrence', file=f2)
 for k, v in dicts.items():
@@ -111,7 +115,7 @@ headers = [
     'review_id',
 ]
 headers.extend(dicts.keys())
-with open(OUT_FILE1, 'w') as csvfile:
+with open(OUT_FILE1, 'w', encoding='utf8') as csvfile:
     csvwriter = csv.DictWriter(csvfile, 
         fieldnames=headers, 
         delimiter=',',
@@ -121,16 +125,19 @@ with open(OUT_FILE1, 'w') as csvfile:
     csvwriter.writeheader()
     
     for row in bow_list:
-        for h in headers:
-            if h not in row:
-                row[h] = 0
-        csvwriter.writerow(row)
+        try:
+            for h in headers:
+                if h not in row:
+                    row[h] = 0
+            csvwriter.writerow(row)
+        except:
+            logging.exception('')
 # Output Tagged BoW CSV
 headers = [
     'review_id',
 ]
 headers.extend(dicts_tag.keys())
-with open(OUT_FILE2, 'w') as csvfile:
+with open(OUT_FILE2, 'w', encoding='utf8') as csvfile:
     csvwriter = csv.DictWriter(csvfile, 
         fieldnames=headers, 
         delimiter=',',
@@ -140,8 +147,11 @@ with open(OUT_FILE2, 'w') as csvfile:
     csvwriter.writeheader()
     
     for row in bow_tag_list:
-        for h in headers:
-            if h not in row:
-                row[h] = 0
-        csvwriter.writerow(row)
+        try:
+            for h in headers:
+                if h not in row:
+                    row[h] = 0
+            csvwriter.writerow(row)
+        except:
+            logging.exception('')
 
